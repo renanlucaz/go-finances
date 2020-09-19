bbd913cfeef2038081415bee2aae2d0c0074abea
unknown <renanluquinhas53@gmail.com>
2020-09-19 00:56:35 -0300

    Challange complete

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,26 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, type, value }: Request): Transaction {
+    const { total } = this.transactionsRepository.getBalance();
+
+    const invalidTransaction = type === 'outcome' && value > total;
+
+    if (!title || !type || !value) {
+      throw Error('Request type is not valid');
+    }
+
+    if (invalidTransaction) {
+      throw Error('You do not have sufficient balance to realize this action');
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      type,
+      value,
+    });
+
+    return transaction;
   }
 }
 
